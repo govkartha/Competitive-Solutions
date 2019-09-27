46e9f804529a911e27055edc8e220134cd4930ae
jesswinw <jesswinw@gmail.com>
2019-09-27 23:34:41 +0530

    Some programs from the space

---
+++ Miscellaneous/multi_tble.py
@@ -0,0 +1,8 @@
+#prints the given number ofmultiples of a given number
+#first input is the number itself 
+#second input is the no of multiples needed
+ 
+num = int(input())
+it = int(input())
+for i in range(it):
+    print(i*num)
