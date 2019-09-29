c8db960566762ac61b2e4b1a59a056a7d1584a2b
jesswinw <jesswinw@gmail.com>
2019-09-29 19:52:39 +0530

    added city_bus(unfinished) and floppy program

---
+++ Miscellaneous/city_bus.py
@@ -0,0 +1,7 @@
+# a bus carries 1,200,000 people each day
+# how many people does the bus carry each day
+# don't use *,/ operator,bitwise operator or loop
+
+p_each = 1200000
+import math
+print(math.sqrt(1200000))
