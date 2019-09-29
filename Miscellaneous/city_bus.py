e35ea3e078c3891b1bf7fd91588888e47e6add0e
jesswinw <jesswinw@gmail.com>
2019-09-29 22:57:57 +0530

    finished city bus pgm

--- Miscellaneous/city_bus.py
+++ Miscellaneous/city_bus.py
@@ -1,7 +1,12 @@
 # a bus carries 1,200,000 people each day
 # how many people does the bus carry each day
 # don't use *,/ operator,bitwise operator or loop
+import time
 
-p_each = 1200000
-import math
-print(math.sqrt(1200000))
+def passen(daypass,iter=1):
+    if(iter==365):
+        return daypass
+    
+    return daypass+passen(daypass,iter+1)
+
+print(passen(1200000))
