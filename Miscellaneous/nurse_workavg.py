5f2c9d308c978b5ece86b9b7a1df053e67edcac6
jesswinw <jesswinw@gmail.com>
2019-09-30 00:31:14 +0530

    added a program which finds the average work hour in a day of a nurse

---
+++ Miscellaneous/nurse_workavg.py
@@ -0,0 +1,13 @@
+#given the no of hours worked by each nurse
+#find the avg which must have a floating point
+
+#the values
+Howard = 8
+Pease = 10
+Campbell = 9
+Grace = 8 
+Maccarthy = 7
+Murphy = 12
+
+print(float((Howard+Pease+Campbell+Grace+Maccarthy+Murphy)/6)) 
+#adding the hours and dividing it with the no of nurses
