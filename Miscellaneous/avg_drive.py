26d686936816e68a5dc19693be29fef3e069fb0c
jesswinw <jesswinw@gmail.com>
2019-10-01 10:00:32 +0530

    avg_rive pgm

---
+++ Miscellaneous/avg_drive.py
@@ -0,0 +1,12 @@
+# Rita drives 2052 miles in 6days stopping at 2 towns each day
+#how many kilometers does she drives between each stop each day
+
+rdrive6 = 2052
+
+drive_eachDay = rdrive6/6
+
+avg_drive = drive_eachDay/2
+km_index = 1.609
+#or
+#avg_drive = ((2052/12)*1.609)
+print(avg_drive*km_index)
---
+++ Miscellaneous/tot_cost3.py
@@ -0,0 +1,17 @@
+# A guy is purchasing gift for his family He purchased:
+# 3 sweaters,each costing $68
+# 1 computer,each costing $75
+# 2 bracelets,each costing $43
+#
+#he returned one bracelet for a full refund and also received a $10 rebate on the computer game
+# What is the total cost of the gifts
+
+#calaculation should be done  in under 3 lines
+
+scost = 68
+ccost = 75
+bcost = 43
+
+#calculations
+total = (3*scost + (ccost-10) + bcost)
+print(total) 
