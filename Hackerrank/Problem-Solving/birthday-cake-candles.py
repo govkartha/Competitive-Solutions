702db38d53d13d0dd5ef5fa53cfba3f01b002c85
jesswinw <jesswinw@gmail.com>
2019-10-13 22:59:28 +0530

    birthday cake problem solution added

---
+++ Hackerrank/Problem-Solving/birthday-cake-candles.py
@@ -0,0 +1,17 @@
+import os
+def birthdayCakeCandles(ar):
+    ar.sort()
+    return(ar.count(ar[-1]))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
