7d312a1ffe8eb4832f523643e1b4b157aaaf7e48
jesswinw <jesswinw@gmail.com>
2020-03-09 16:04:01 +0530

    finished Athletic sort

---
+++ Hackerrank/Python/Athlete-Sort.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+if __name__ == '__main__':
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    k = int(input())
+    
+    for i in sorted(arr,key=lambda x:x[k]):
+        for j in i:
+            print(j,end=' ')
+        print()
