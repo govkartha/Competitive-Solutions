eba1f97288eb8ae668b1391d84426db36f41d970
jesswinw <jesswinw@gmail.com>
2020-05-09 00:02:32 +0530

    Day 0 Finished 😁

---
+++ 10-days-of-Js/Day-0:Data-Types.js
@@ -0,0 +1,14 @@
+function performOperation(secondInteger, secondDecimal, secondString) {
+  const firstInteger = 4;
+
+  const firstDecimal = 4.0;
+
+  const firstString = "HackerRank ";
+
+  //The lines of code used is below
+  console.log(firstInteger + parseInt(secondInteger));
+
+  console.log(firstDecimal + parseFloat(secondDecimal));
+
+  console.log(firstString + secondString);
+}
