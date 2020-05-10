9d5d8f64aca83d0bbc96dd03b0e768e5d35cd7b5
jesswinw <jesswinw@gmail.com>
2020-05-11 00:15:14 +0530

    Day 4 partially completed ⚒

---
+++ 10-days-of-Js/Day-4:CreateaRectangleObject.js
@@ -0,0 +1,12 @@
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+  const rect = {
+    length: a,
+    width: b,
+    perimeter: 2 * (a + b),
+    area: a * b,
+  };
+  return rect;
+}
