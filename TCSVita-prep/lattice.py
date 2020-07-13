d389cfb99efc1344c35390cf64500dcbafbc527c
jesswinw <jesswinw@gmail.com>
2020-07-13 18:44:03 +0530

    😀finds the no of routes of n*n matrix and now uses built in fact function

--- TCSVita-prep/lattice.py
+++ TCSVita-prep/lattice.py
@@ -1,13 +1,10 @@
-def fact(n):
-    if(n == 1):
-        return 1
-    return n * fact(n-1)
+from math import factorial as fact
 
 
 # arg size is the size of the square grid
-def getroutes(size):
-    return fact(2*size)//(fact(size)**2)
+def getroutes(rows, cols):
+    return fact((rows+cols))//(fact(rows)*fact(cols))
 
 
 # for 20*20 grid
-print(getroutes(20))
+print(getroutes(20, 20))
