91aca19ed59d6b735ec55cae0255a2ba4bfee032
Govind Kartha <44610432+govkartha@users.noreply.github.com>
2020-10-02 00:31:55 +0530

    alternate solution

--- Hackerrank/Python/map-and-lambda-expression.py
+++ Hackerrank/Python/map-and-lambda-expression.py
@@ -1,16 +1,12 @@
 cube = lambda x: x**3 # complete the lambda function 
 
 def fibonacci(n):
-    lis = []
-    if(n==1):
-        lis +=[0]
-    elif(n!=0):
-        lis += [0,1]
-        for i in range(2,n):
-            lis.append(lis[-1]+lis[-2])
-    return lis
+    a,b = 0,1
+    for i in range(n):
+        yield a
+        a,b = b,a+b
     # return a list of fibonacci numbers
 
 if __name__ == '__main__':
     n = int(input())
-    print(list(map(cube, fibonacci(n))))
+    print(list(map(cube, list(fibonacci(n)))))
